c893334d4edfc5678dd963f52998b1ac409e1869
Alex Kamedov <alex@kamedov.ru>
2012-09-19 14:44:38 +0600

    remove hard links with django.contrib.auth.models.User

--- djangoratings/default_settings.py
+++ djangoratings/default_settings.py
@@ -2,4 +2,6 @@ from django.conf import settings
 
 # Used to limit the number of unique IPs that can vote on a single object+field.
 #   useful if you're getting rating spam by users registering multiple accounts
-RATINGS_VOTES_PER_IP = 3
+RATINGS_VOTES_PER_IP = getattr(settings, 'RATINGS_VOTES_PER_IP', 3)
+
+RATINGS_USER_MODEL = getattr(settings, 'RATINGS_USER_MODEL', 'auth.User')
